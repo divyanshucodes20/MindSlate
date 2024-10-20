aba8a2951618c978a7216f7debe46601e628f2c6
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-20 11:30:39 +0530

    Refactor database connection and update port number

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,19 +2,25 @@ const express=require('express')
 const router=express.Router()
 const User=require('../models/User.models.js')
 const {body,validationResult}=require('express-validator')
-router.post('/',[
+router.post('/registerUser',[
     body('email','Enter valid Email').isEmail(),
     body('username','Enter valid name minimum length of 3').isLength({min:3}),
     body('password','Enter valid Password minimum length of 5').isLength({min:5})
-],(req,res)=>{
+],async(req,res)=>{
     const errors=validationResult(req)
     if(!errors.isEmpty()){
                  return res.status(400).json({errors:errors.array()})
     }
-    User.create({
+    let user=await User.findOne({email:req.body.email,username:req.body.username}) 
+    if(user){
+       return res.status(400).json({error:"User with this email or username already exists"}) 
+    }
+    user=await User.create({
         username:req.body.username,
         password:req.body.password,
         email:req.body.email
-    }).then(user=>res.json(user))
+    })
+    res.status(200)
+    res.json(user)
 })
 module.exports=router
